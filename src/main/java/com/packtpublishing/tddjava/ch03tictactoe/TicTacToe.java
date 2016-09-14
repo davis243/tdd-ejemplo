7ecb116d94379011eaeb97b77f1afe0c4722ada0
dsarmiento <dsarmiento@mef.gob.pe>
2016-09-14 09:46:30 -0500

    Add jacoco and tests are finished

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -4,11 +4,84 @@ package com.packtpublishing.tddjava.ch03tictactoe;
  * Created by dsarmiento on 12/09/2016.
  */
 public class TicTacToe {
-    public void play(int x ,int y)
+    private Character[][] board = {{'\0', '\0', '\0'},
+            {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+
+    private char lastPlayer = '\0';
+    private static final int SIZE = 3;
+    public String play(int x ,int y)
     {
-        if(x < 1 || x > 3)
-            throw new RuntimeException("X is outside board");
-        if(y < 1 || y > 3)
-            throw new RuntimeException("Y is outside board");
+        checkAxis(x);
+        checkAxis(y);
+        lastPlayer = nextPlayer();
+        setBox( x, y,lastPlayer);
+
+        if(isWin(x, y ))
+            return    lastPlayer + " is the winner";
+        else if (isDraw()) {
+            return "The result is draw";
+        }
+        else
+            return "No winner";
+
     }
+    private boolean isDraw() {
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (board[x][y] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isWin(int x , int y)
+    {
+        int playerTotal = lastPlayer * SIZE;
+        char  horizontal = '\0', vertical = '\0';
+        char diagonalTopBottom = '\0',diagonalBottomTop='\0';
+
+
+        for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
+            diagonalTopBottom += board[i][i] ;
+            diagonalBottomTop += board[i][SIZE-i-1] ;
+
+        }
+
+        if (horizontal == playerTotal ||
+            vertical == playerTotal ||
+            diagonalTopBottom == playerTotal ||
+            diagonalBottomTop == playerTotal)
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public void checkAxis(int axis)
+    {
+        if(axis < 1 || axis > 3)
+            throw new RuntimeException("X is outside board");
+    }
+    public void setBox(int x,int y, char lastPlayer)
+    {
+        if(board[x - 1][y - 1] != '\0')
+            throw new RuntimeException("Box is occupied");
+        else
+            board[x - 1][y - 1] = lastPlayer;
+    }
+    public char nextPlayer()
+    {
+        if(lastPlayer=='X')
+            lastPlayer = 'O';
+        else
+            lastPlayer = 'X';
+
+        return lastPlayer;
+    }
+
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,9 +1,13 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+
 
 /**
  * Created by dsarmiento on 12/09/2016.
@@ -31,4 +35,79 @@ public class TicTacToeSpec {
         exception.expect(RuntimeException.class);
         ticTacToe.play(2, 5);
     }
+    @Test
+    public void whenOccupiedThenRuntimeException()
+    {
+        ticTacToe.play(2,1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2,1);
+    }
+    @Test
+    public void givenFirstTurnWhenNextPlayerThenX() {
+        assertEquals('X', ticTacToe.nextPlayer());
+    }
+    @Test
+    public void givenLastTurnWasXWhenNextPlayerThenO()
+    {
+        ticTacToe.play(1, 1);
+        assertEquals('O', ticTacToe.nextPlayer());
+    }
+    @Test
+    public void whenPlayThenNoWinner()
+    {
+        String actual = ticTacToe.play(1,1);
+        assertEquals("No winner", actual);
+    }
+    @Test
+    public void whenPlayAndWholeHorizontalLineThenWinner() {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(2, 2); // O
+        String actual = ticTacToe.play(3, 1); // X
+        assertEquals("X is the winner", actual);
+    }
+    @Test
+    public void whenPlayAndWholeVerticalLineThenWinner() {
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(1, 1); // O
+        ticTacToe.play(3, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 2); // X
+        String actual = ticTacToe.play(1, 3); // O
+        assertEquals("O is the winner", actual);
+    }
+    @Test
+    public void whenPlayAndTopBottomDiagonalLineThenWinner() {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 2); // X
+        ticTacToe.play(1, 3); // O
+        String actual = ticTacToe.play(3, 3); // X
+        assertEquals("X is the winner", actual);
+    }
+
+    @Test
+    public void whenPlayAndBottomTopDiagonalLineThenWinner() {
+        ticTacToe.play(3, 1); // X
+        ticTacToe.play(1, 1); // O
+        ticTacToe.play(2, 2); // X
+        ticTacToe.play(1, 2); // O
+        String actual =  ticTacToe.play(1, 3);// X
+        assertEquals("X is the winner", actual);
+    }
+    @Test
+    public void whenAllBoxesAreFilledThenDraw() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(1, 3);
+        ticTacToe.play(2, 1);
+        ticTacToe.play(2, 3);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(3, 1);
+        ticTacToe.play(3, 3);
+        String actual = ticTacToe.play(3, 2);
+        assertEquals("The result is draw", actual);
+    }
+
 }
